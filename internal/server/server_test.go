d0296f26dac852f0246fbc73b07a08de7573ccec
rustieepo0 <adityaasilwal@gmail.com>
2024-05-27 20:05:04 +0530

    Add: TestStatusHandeler

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,4 +12,15 @@ func TestNewServer(t *testing.T) {
 	server, err := New(":8000", "v1", "http://localhost:1984")
 	assert.NoError(t, err)
 	assert.NotNil(t, server)
+}
+
+func TestStatusHandeler(t *testing.T) {
+	server, _ := New(":8080", "v1", "http://localhost:1984")
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	server.server.Handler.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), `{"status":"ok"}`)
 }
