053630aae0403ec32bda38a51ba0dcca01e0650e
rustieepo0 <adityaasilwal@gmail.com>
2024-05-27 19:55:39 +0530

    NewServerTest

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,5 +1,13 @@
 package server
 
-type responseError struct {
-	Error string `json:"error"`
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewServer(t *testing.T) {
+	server, err := New(":8000", "v1", "http://localhost:1984")
+	assert.NoError(t, err)
+	assert.NotNil(t, server)
 }
